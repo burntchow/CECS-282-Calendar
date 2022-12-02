6e46a777e0fec1f93c3b2a7bc8bacf56ae657b2a
ave <72708879+burntchow@users.noreply.github.com>
2022-12-01 17:34:40 -0800

    updated files

---
+++ myDate.h
@@ -0,0 +1,34 @@
+// Name: Aveline Villaganas
+// Class (CECS 282-04)
+// Project Name (Program 2 - myDate Object)
+// Due Date (3/1/2022)
+//
+// I certify that this program is my own original work. I did not copy any part of this program from any other source. 
+// I further certify that I typed each and every line of code in this program.
+
+#ifndef myDate_hpp
+#define myDate_hpp
+#include <string>
+using namespace std;
+
+class myDate{
+  private:
+    int month;
+    int day;
+    int year; 
+  public:
+    myDate();
+    myDate(int M, int D, int Y);
+    void display();
+    void increaseDate(int N);
+    void decreaseDate(int N);
+    void addMonth();
+    int daysBetween(myDate D);
+    int getMonth();
+    int getDay();
+    int getYear();
+    int dayOfYear();
+    string dayName();
+};
+
+#endif
